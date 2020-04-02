e3bf9fbb4f12d27fc59399e4b9c1b9715a78e665
yahuan zhu <yahuanzhu@gmail.com>
2020-04-02 03:13:08 -0400

    simple map

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+import React from "react";
+import ReactDOM from "react-dom";
+import Init from './init';
+
+
+ ReactDOM.render(
+	 <Init />,
+
+	document.getElementById('root')
+	);
---
+++ src/init.js
@@ -0,0 +1,14 @@
+import MapChart from './components/MapChart';
+import React from 'react';
+
+export default function Init() {
+    // receiving file data from main.js
+
+return (
+      <div className="Init" >
+        <div class="mapchart">
+            <MapChart/>
+        </div>         
+      </div>
+    );
+}

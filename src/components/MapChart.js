4e5599a9a8b69c07e992a2285f91d088a69169d0
haoranx3 <haoranx3@vt.edu>
2020-04-18 20:06:14 -0400

    react front end

--- src/components/MapChart.js
+++ src/components/MapChart.js
@@ -7,67 +7,54 @@ import {
   Marker,
   Annotation
 } from "react-simple-maps";
-
+import { scaleQuantile } from "d3-scale";
+import { useEffect, useState } from "react";
+import { csv } from "d3-fetch";
 import allStates from "../data/allstates.json";
 
-const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
 
-const offsets = {
-  VT: [50, -8],
-  NH: [34, 2],
-  MA: [30, -1],
-  RI: [28, 2],
-  CT: [35, 10],
-  NJ: [34, 1],
-  DE: [33, 0],
-  MD: [47, 10],
-  DC: [49, 21]
-};
+const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json";
+
 
 const MapChart = () => {
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    csv("/unemployment-by-county-2017.csv").then(county => {
+      setData(county);
+    })
+  }, []);
+
+  const colorScale = scaleQuantile()
+      .domain(data.map(d => d.unemployment_rate))
+      .range([
+        "#ffedea",
+        "#ffcec5",
+        "#ffad9f",
+        "#ff8a75",
+        "#ff5533",
+        "#e2492d",
+        "#be3d26",
+        "#9a311f",
+        "#782618"
+      ])
+
   return (
     <ComposableMap projection="geoAlbersUsa">
       <Geographies geography={geoUrl}>
-        {({ geographies }) => (
-          <>
-            {geographies.map(geo => (
+        {({ geographies }) => 
+          geographies.map(geo => {
+            const cur = data.find(s => s.id === geo.id);
+            console.log(data)
+            return (
               <Geography
                 key={geo.rsmKey}
-                stroke="#FFF"
                 geography={geo}
-                fill="#DDD"
+                fill={cur ? colorScale(cur.unemployment_rate) : "#EEE"}
               />
-            ))}
-            {geographies.map(geo => {
-              const centroid = geoCentroid(geo);
-              const cur = allStates.find(s => s.val === geo.id);
-              return (
-                <g key={geo.rsmKey + "-name"}>
-                  {cur &&
-                    centroid[0] > -160 &&
-                    centroid[0] < -67 &&
-                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
-                      <Marker coordinates={centroid}>
-                        <text y="2" fontSize={14} textAnchor="middle">
-                          {cur.id}
-                        </text>
-                      </Marker>
-                    ) : (
-                      <Annotation
-                        subject={centroid}
-                        dx={offsets[cur.id][0]}
-                        dy={offsets[cur.id][1]}
-                      >
-                        <text x={4} fontSize={14} alignmentBaseline="middle">
-                          {cur.id}
-                        </text>
-                      </Annotation>
-                    ))}
-                </g>
-              );
-            })}
-          </>
-        )}
+            );
+          })
+        }
       </Geographies>
     </ComposableMap>
   );

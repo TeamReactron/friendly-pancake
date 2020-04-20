05497a45c5ea4fb400b757c7ff74e8e394df0c56
yahuan zhu <yahuanzhu@gmail.com>
2020-04-19 20:13:39 -0400

    able to read input field

--- src/init.js
+++ src/init.js
@@ -1,14 +1,21 @@
 import MapChart from './components/MapChart';
 import React from 'react';
-
 export default function Init() {
     // receiving file data from main.js
-
+    // var countytext = document.getElementById('county');
+    // var statetext = document.getElementById('state');
+    // var accidenttext = document.getElementById('accident'); 
+    // var temperaturetext =  document.getElementById('temperature').value;
+    // var humiditytext =  document.getElementById('humidity').value;
+    // function predictionClick(){
+    //   console.log(temperaturetext);
+    //   console.log(humiditytext);
+    // }
 return (
       <div className="Init" >
         <div class="mapchart">
             <MapChart/>
-        </div>         
+        </div>    
       </div>
     );
 }

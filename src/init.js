19da04530a585718f1558d4a65b746876f10f03f
yahuan zhu <yahuanzhu@gmail.com>
2020-04-19 20:16:06 -0400

    remove stuff

--- src/init.js
+++ src/init.js
@@ -2,15 +2,7 @@ import MapChart from './components/MapChart';
 import React from 'react';
 export default function Init() {
     // receiving file data from main.js
-    // var countytext = document.getElementById('county');
-    // var statetext = document.getElementById('state');
-    // var accidenttext = document.getElementById('accident'); 
-    // var temperaturetext =  document.getElementById('temperature').value;
-    // var humiditytext =  document.getElementById('humidity').value;
-    // function predictionClick(){
-    //   console.log(temperaturetext);
-    //   console.log(humiditytext);
-    // }
+
 return (
       <div className="Init" >
         <div class="mapchart">

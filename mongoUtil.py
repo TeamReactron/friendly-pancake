70de445444943fc881d6262de12a3b29a1b05441
cccecilia <jliu738@gatech.edu>
2020-04-20 18:26:39 -0400

    query script

--- mongoUtil.py
+++ mongoUtil.py
@@ -3,8 +3,8 @@ import csv
 from pprint import pprint
 from pymongo import InsertOne, DeleteMany, ReplaceOne, UpdateOne, WriteConcern
 from pymongo.errors import BulkWriteError
-import os
-from kaggle.api.kaggle_api_extended import KaggleApi
+# import os
+# from kaggle.api.kaggle_api_extended import KaggleApi
 
 
 
@@ -12,8 +12,8 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 # match_collection = match_connection[]
 
 
-dataset_path = os.getcwd()
-file_name = 'US_Accidents_Dec19.csv'
+# dataset_path = os.getcwd()
+# file_name = 'US_Accidents_Dec19.csv'
 
 client = MongoClient('localhost', 27017)
 # os.getenv("MONGODB_CONFIG")
@@ -35,6 +35,8 @@ def writeAccidentToMongo(arr):
         try:
             db.accidentCollection.bulk_write([
                 # InsertOne({'_id':teleArr[2]}),
+                # date = arr[i][22].split(' ')[0]
+                # time = arr[i][22].split(' ')[0]
                 InsertOne({
                     'ID': arr[i][0],
                     'Source': arr[i][1],
@@ -57,7 +59,8 @@ def writeAccidentToMongo(arr):
                     'Country': arr[i][19],
                     'Timezone': arr[i][20],
                     'Airport_Code': arr[i][21],
-                    'Weather_Timestamp': arr[i][22],
+                    'Weather_Timestamp_Date': arr[i][22].split(' ')[0],
+                    # 'Weather_Timestamp_Time': arr[i][22].split(' ')[1],
                     'Amenity': arr[i][32],
                     'Bump': arr[i][33],
                     'Crossing': arr[i][34],
@@ -75,7 +78,7 @@ def writeAccidentToMongo(arr):
                     'Civil_Twilight': arr[i][46],
                     'Nautical_Twilight': arr[i][47],
                     'Astronomical_Twilight': arr[i][48],
-                    }) for i in range(len(arr) - 1)
+                    }) for i in range(len(arr))
                 ])
         except BulkWriteError as bwe:
             pprint(bwe.details)
@@ -119,7 +122,7 @@ def writeWeatherToMongo(arr):
                     'Civil_Twilight': arr[i][46],
                     'Nautical_Twilight': arr[i][47],
                     'Astronomical_Twilight': arr[i][48],
-                    }) for i in range(len(arr) - 1)
+                    }) for i in range(len(arr))
                 ])
         except BulkWriteError as bwe:
             pprint(bwe.details)
@@ -148,53 +151,42 @@ def writeAveWeatherToMongo(arr):
 
 
 if __name__ == "__main__":
-    # file = open('./US_Accidents_Dec19.csv', 'r')
-    # print(file.readline())
-    # for line in file:
-        
-    #     sline = line.split(',')
-    #     teleArr.append(sline)
-    
-    
-    # weatherArr = {(county, date): [[temp1, temp2...], [chill1, chill2...]]}
 
-    # file.close()
-    if os.path.exists(dataset_path + '/' + file_name) == False:
-        api = KaggleApi()
-        api.authenticate()
-        api.dataset_download_files('sobhanmoosavi/us-accidents',path = dataset_path,unzip=True)
+    # if os.path.exists(dataset_path + '/' + file_name) == False:
+    #     api = KaggleApi()
+    #     api.authenticate()
+    #     api.dataset_download_files('sobhanmoosavi/us-accidents',path = dataset_path,unzip=True)
 
 
     with open('US_Accidents_Dec19.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            date = row[22].split(' ')[0]
-            # print(date)
-            if row[16] is not '' and row[23] is not '' and row[25] is not '' and row[26] is not '' and row[27] is not '':
-                # print(row)
-                # print(float(row[23]), float(row[25]), float(row[26]), float(row[27]), [float(row[30])] if row[30] is not '' else 0)
-                if ((row[16], date)) not in weatherArr:
-                    percip = float(row[30]) if row[30] is not '' else 0
-                    weatherArr[row[16], date] = [[float(row[23])], [float(row[25])], [float(row[26])], [float(row[27])], [percip]]
-                else:
-                    # print((row[16], date))
-                    weatherArr[row[16], date][0].append(float(row[23]))
-                    weatherArr[row[16], date][1].append(float(row[25]))
-                    weatherArr[row[16], date][2].append(float(row[26]))
-                    weatherArr[row[16], date][3].append(float(row[27]))
-                    weatherArr[row[16], date][4].append(percip)
+
+            # parse accident
             # sline = row.split(',')
-            # teleArr.append(row)
-        for key in weatherArr.keys():
-            # print([key[0], key[1], sum(weatherArr[key][0])/len(weatherArr[key][0]), 
-            #     sum(weatherArr[key][1])/len(weatherArr[key][1]), sum(weatherArr[key][2])/len(weatherArr[key][2])])
-            # print([weatherArr[key][3], weatherArr[key][4]])
-            aveWeatherArr.append([key[0], key[1], sum(weatherArr[key][0])/len(weatherArr[key][0]), 
-                sum(weatherArr[key][1])/len(weatherArr[key][1]), sum(weatherArr[key][2])/len(weatherArr[key][2]), 
-                sum(weatherArr[key][3])/len(weatherArr[key][3]), sum(weatherArr[key][4])/len(weatherArr[key][4])])
-        # print(aveWeatherArr)
-    # writeAccidentToMongo(teleArr)
+            teleArr.append(row)
+
+            #parse weather
+        #     date = row[22].split(' ')[0]
+        #     if row[16] is not '' and row[23] is not '' and row[25] is not '' and row[26] is not '' and row[27] is not '':
+        #         if ((row[16], date)) not in weatherArr:
+        #             percip = float(row[30]) if row[30] is not '' else 0
+        #             weatherArr[row[16], date] = [[float(row[23])], [float(row[25])], [float(row[26])], [float(row[27])], [percip]]
+        #         else:
+        #             weatherArr[row[16], date][0].append(float(row[23]))
+        #             weatherArr[row[16], date][1].append(float(row[25]))
+        #             weatherArr[row[16], date][2].append(float(row[26]))
+        #             weatherArr[row[16], date][3].append(float(row[27]))
+        #             weatherArr[row[16], date][4].append(percip)
+
+        # for key in weatherArr.keys():
+        #     aveWeatherArr.append([key[0], key[1], sum(weatherArr[key][0])/len(weatherArr[key][0]), 
+        #         sum(weatherArr[key][1])/len(weatherArr[key][1]), sum(weatherArr[key][2])/len(weatherArr[key][2]), 
+        #         sum(weatherArr[key][3])/len(weatherArr[key][3]), sum(weatherArr[key][4])/len(weatherArr[key][4])])
+
+
+
+    writeAccidentToMongo(teleArr)
     # writeWeatherToMongo(teleArr)
-    writeAveWeatherToMongo(aveWeatherArr)
-    # print(teleArr)
+    # writeAveWeatherToMongo(aveWeatherArr)
     # db.accidentCollection.drop()

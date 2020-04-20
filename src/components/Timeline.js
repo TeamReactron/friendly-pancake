73a8abfa46d905ca0f5624d10806f2a0f08812b9
haoranx3 <haoranx3@vt.edu>
2020-04-19 21:50:12 -0400

    timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,35 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 
-function valuetext(value) {
-  return `${value}`;
-}
 
-const useStyles = makeStyles({
-    root: {
-      width: 30,
-    },
-  });
+const Timeline = () => {
+  // Declare a new state variable, which we'll call "count"
+  const [year, setYear] = useState(2000);
 
-export default function timeline() {
+  const handleChange = event => {
+      //console.log(event.target.ariaValueNow);
+      setYear(event.target.ariaValueText);
+      console.log(event)
+  }
+
+  function valuetext(value) {
+    setYear(value)
+    return `${value}`;
+  }
 
   return (
     <div>
-      <Typography gutterBottom>
-        Timeline
+      <Typography id="timeline" gutterBottom>
+        Year: {year}
       </Typography>
       <Slider
         defaultValue={2000}
         getAriaValueText={valuetext}
-        aria-labelledby="year"
-        step={2}
+        aria-labelledby="timeline"
+        step={1}
         marks
         min={2000}
         max={2025}
         valueLabelDisplay="auto"
+        onChange={event => handleChange(event)}
       />
     </div>
   );
 }
+export default Timeline;
